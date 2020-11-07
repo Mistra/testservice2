93d4aab2bd5ed3ed0c0768fc82ae9813756c3925
Mistra <sperectrum@gmail.com>
2020-11-07 10:49:19 +0100

    WIP

---
+++ src/main/java/net/endu/enduscan/endpoint/EndpointController.java
@@ -0,0 +1,34 @@
+package net.endu.enduscan.endpoint;
+
+import java.time.Duration;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping(value = "/surface")
+public class EndpointController {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.setConnectTimeout(Duration.ofSeconds(2000)).setReadTimeout(Duration.ofSeconds(2000)).build();
+    }
+
+    @GetMapping()
+    public String operatorIsAllowed(RestTemplate restTemplate) {
+        String str = null;
+        try {
+            str = restTemplate.getForObject("http://service1:8762/backend", String.class);
+        } catch (RestClientException e) {
+            str = "Cannot fetch service";
+        }
+        str = str  + " - test!!!";
+        return str;
+    }
+
+}
